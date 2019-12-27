134b8e6d8edffa4b1fc8172b79bba485d1008cc9
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-27 01:59:14 -0500

    init commit

---
+++ AnimationApp/Controller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Esmaeil MIRZAEE on 27/12/2019.
+//  Copyright © 2019 Esmaeil MIRZAEE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
